2a6f95eb581ee9d904d3e9ed26cc984255e7b905
elianagsilva <105246709+elianagsilva@users.noreply.github.com>
2022-06-09 23:00:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let nome = "Eliana";
+
+let number = 22;
+
+let ano = 2022
+
+let sub = (ano - number)
+
+console.log (number + ano);
+
+console.log("Estou mexendo no console.log");
+
+const quartaFeira= true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
+
+
+
+
